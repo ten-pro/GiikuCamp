8d18aef094c445e546f2931800790b6488565c86
明石凜太 <jeffrey.bezos@icloud.com>
2022-12-10 14:41:52 +0900

    kkk

--- giikucamp/src/components/Setting.vue
+++ giikucamp/src/components/Setting.vue
@@ -15,6 +15,71 @@
         <div class="error">
             <div class="uerror">この名前はすでに使用されています。</div>
         </div>
+        <div class="bigbox"> </div>
+        <div class="push">
+            <div class="pusha">通知</div>
+            <!-- toggle-button -->
+            <div class="button r" id="button-1">
+                <input type="checkbox" class="checkbox" />
+                <div class="knobs"></div>
+                <div class="layer"></div>
+            </div>
+            <!--  -->
+        </div>
+        <div class="times">
+            <div class="timetxt">時間</div>
+            <!--  -->
+
+            <select class="timese">
+                <option value="--">--</option>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+            </select>
+
+            <!--  -->
+        </div>
+        <div class="checkday">
+            <div class="checkn">日程</div>
+
+            <div class="checkma">
+                <label for="horns1" class="checkma">
+                    <input type="checkbox" id="horns1">1日前
+                </label>
+
+                <label for="horns2" class="checkma">
+                    <input type="checkbox" id="horns2">3日前
+                </label>
+                <label for="horns3" class="checkma">
+                    <input type="checkbox" id="horns3">5日前
+                </label>
+                <label for="horns4" class="checkma">
+                    <input type="checkbox" id="horns4">7日前
+                </label>
+            </div>
+
+        </div>
     </div>
 </template>
 <script setup>
@@ -26,7 +91,7 @@ import Header from "./Header.vue"
     color: #E27A93;
 }
 .sheader{
-    padding-left: 40px; 
+padding:20px 0px 20px 40px;
 }
 .username{
     font-family: sans-serif;
@@ -43,9 +108,10 @@ import Header from "./Header.vue"
     background-color: #D9D9D9;
     border-radius: 10px;
     height: 40px;
-    width:130px;
+    width:100px;
     border-style:none;
     font-size: large;
+    padding:0 10px;
 }
 .namebox::placeholder {
     color: #E27A93;
@@ -55,10 +121,175 @@ text-align: center;
   .uerror{
     font-size: 12px;
     color: red;
+    padding-right:20px;
   }
   .error{
     text-align: right;
-    padding-top:
+    padding-top:5px;
   }
+  .pusha{
+    font-family: sans-serif;
+    font-weight: 1000;
+    color:#E27A93;
+    font-size: large;
+  }
+.push{
+    align-items: center;
+    display: flex;
+    justify-content: space-around;
+    padding: 30px 0px;
+}
+.bigbox{
+position:absolute;
+background-color:transparent;
+    width:350px;
+    height:250px;
+    left:30px;
+    top:300px;
+    border-radius: 20px;
+
+    border-left:1px solid #E27A93;
+border-bottom:1px solid #E27A93;
+border-right:1px solid #E27A93;
+}
+.times{
+    display: flex;
+    margin-top:10px;
+    justify-content: space-around;
+}
+.timetxt{
+    font-family: sans-serif;
+    font-weight: 1000;
+    color:#E27A93;
+    font-size: large;
+    margin-left:10px;
+}
+.timese{
+    width:100px;
+    height:30px;
+    color:#ffffff;
+    font-size: large;
+    background-color:#E27A93
+}
+.checkday{
+display: flex;
+justify-content: space-around;
+margin-top:40px;
+}
+.checkma{
+    display:block;
+    color:#E27A93;
+    font-size:large;
+    font-weight: 600;
+    font-family: sans-serif;
+}
+.checkn{
+    font-family: sans-serif;
+    font-weight: 1000;
+    color:#E27A93;
+    font-size: large;
+    margin-right:10px;
+}
+input[type=checkbox] {
+	transform: scale(2);
+	margin: 0 6px 0 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*toggle-button*/
+.knobs,
+.layer {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.button {
+  position: relative;
+  top: 50%;
+  width: 74px;
+  height: 36px;
+  overflow: hidden;
+}
+
+.button.r,
+.button.r .layer {
+  border-radius: 100px;
+}
+
+.button {
+  border-radius: 2px;
+}
+
+.checkbox {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  opacity: 0;
+  cursor: pointer;
+  z-index: 3;
+}
+
+.knobs {
+  z-index: 2;
+}
+
+.layer {
+  width: 100%;
+  background-color: #D9D9D9;
+  transition: 0.3s ease all;
+  z-index: 1;
+}
+#button-1 .knobs:before {
+  content: "OFF";
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  width: 20px;
+  height: 10px;
+  color: #fff;
+  font-size: 10px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 1;
+  padding: 9px 4px;
+  background-color: #E27A93;
+  border-radius: 50%;
+  transition: 0.3s cubic-bezier(0.18, 0.89, 0.35, 1.15) all;
+}
+
+#button-1 .checkbox:checked + .knobs:before {
+  content: "NO";
+  left: 42px;
+  background-color: #E27A93;
+}
+
+#button-1 .checkbox:checked ~ .layer {
+  background-color: #E27A93;
+  opacity: 0.3;
+}
+
+#button-1 .knobs,
+#button-1 .knobs:before,
+#button-1 .layer {
+  transition: 0.3s ease all;
+}
+
 </style>
   
